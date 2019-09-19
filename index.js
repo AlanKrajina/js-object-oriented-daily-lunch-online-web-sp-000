689b5d133b5bd54cd5162709f00a9933c4448321
Alan Krajina <alankrajina88@gmail.com>
2019-09-19 11:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0;
 let mealId = 0;
+let customerId = 0;
 
 class Neighborhood {
   constructor(name){
@@ -9,6 +10,28 @@ class Neighborhood {
     this.id = ++neighborhoodId
 
     store.neighborhoods.push(this)
+  }
+}
+
+
+class Meal {
+  constructor(text,numbr){
+    this.text = text
+    this.numbr = numbr
+    this.id = ++mealId
+
+    store.meals.push(this)
+  }
+}
+
+
+class Customer {
+  constructor(name,neighborhood){
+    this.name = name
+    this.numbr = numbr
+    this.id = ++customerId
+
+    store.meals.push(this)
   }
 }
 
