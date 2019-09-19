878da12125003305cabc80743ef79a1851af98e5
Alan Krajina <alankrajina88@gmail.com>
2019-09-19 11:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0;
-
+let mealId = 0,
 
 class Neighborhood {
   constructor(name){
     this.name = name
     this.id = ++neighborhoodId
+
+    store.neighborhoods.push(this)
+  }
+}
+
+
+class Meal {
+  constructor(text,numb){
+    this.text = text
+    this.numb = numb
+    this.id = ++mealId
 
     store.neighborhoods.push(this)
   }
